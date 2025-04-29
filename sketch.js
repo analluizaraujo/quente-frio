708dc1148a7cd779b396bb783a220fda975049ec
analluizaraujo <ana.araujo.santos21@escola.pr.gov.br>
2025-04-29 11:43:12 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let x;
+let y;
+
+
+function setup() {
+  createCanvas(400, 400);
+ x = random(400);
+ x = int(x);
+ y = random(400);
+ y = int(y);
+}
+function draw() {
+  background("black")
+x = x + random(-20,20);
+y = y + random(-20,20);
+  if (x > 400) {
+    x = 400;
+  }
+  let dintancia;
+  //distancia =sqrt(distanciaX*distanciaX + distanciaY*distanciaY);
+  distancia = dist(mouseX,mouseY,x,y)
+  circle(mouseX,mouseY,distancia);
+  circle(x,y,10);
+  fill("pink")
+  
+ if(distancia< 3);
+ if(mouseX== x && mouseY ==y) {
+text("Encontrei!",200,200)
+noLoop ()
+  }
+  }
